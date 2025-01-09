59982d56bb5815bfa60d3597165edfce49e9e8a6
Borovski Cergei <borovskicerg0001@gmaol.com>
2025-01-09 14:55:57 +0200

    build success

---
+++ src/main/java/com/example/demo/OnlineBookStoreApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+
+    @Bean
+public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book peacefulWarrior = new Book();
+            peacefulWarrior.setAuthor("Milman");
+            peacefulWarrior.setTitle("Peaceful warrior");
+            peacefulWarrior.setIsbn("xxx");
+            peacefulWarrior.setPrice(BigDecimal.valueOf(999));
+            bookService.save(peacefulWarrior);
+        };
+    }
+
+}
